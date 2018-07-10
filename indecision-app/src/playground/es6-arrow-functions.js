cad6c44c9840aed89c71c7636c58f734dedcfe02
Dennis van Loon <dennis@Ds-MacBook-Pro.local>
2018-07-10 16:42:22 +0200

    after college 14

--- indecision-app/src/playground/es6-arrow-functions.js
+++ indecision-app/src/playground/es6-arrow-functions.js
@@ -1,6 +1,4 @@
-'use strict';
-
-function square(x) {
+function square (x) {
   return x * x;
 }
 console.log(square(3));
@@ -8,15 +6,11 @@ console.log(square(3));
 // const squareArrow = (x) => {
 //   return x * x;
 // };
-var squareArrow = function squareArrow(x) {
-  return x * x;
-};
+const squareArrow = (x) => x * x;
 console.log(squareArrow(4));
 
 // const getFirstName = (fullName) => {
 //   return fullName.split(' ')[0];
 // }
-var getFirstName = function getFirstName(fullName) {
-  return fullName.split(' ')[0];
-};
+const getFirstName = (fullName) => fullName.split(' ')[0];
 console.log(getFirstName('Mike Smith'));
