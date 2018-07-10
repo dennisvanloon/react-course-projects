3f42d9dd84615d557b80245af62d1a7634712ad1
Dennis van Loon <dennis@Ds-MacBook-Pro.local>
2018-07-10 15:58:04 +0200

    after college 11

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -2,18 +2,29 @@
 
 console.log('Indecision app js running');
 
+var app = {
+  title: 'This is the indecision app',
+  subtitle: 'I hope you like it',
+  options: ['One', 'Two']
+};
+
 var template = React.createElement(
   'div',
   null,
   React.createElement(
     'h1',
     null,
-    'Welcome again to the Indecision App'
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
   ),
   React.createElement(
     'p',
     null,
-    'This is some info'
+    app.options && app.options.length > 0 ? 'Here are your options' : 'No options'
   ),
   React.createElement(
     'ol',
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,9 +1,16 @@
 console.log('Indecision app js running');
 
+var app = {
+  title: 'This is the indecision app',
+  subtitle: 'I hope you like it',
+  options: ['One', 'Two']
+};
+
 var template = (
   <div>
-    <h1>Welcome again to the Indecision App</h1>
-    <p>This is some info</p>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{(app.options && app.options.length > 0) ? 'Here are your options' : 'No options'}</p>
     <ol>
       <li>Item One</li>
       <li>Item Two</li>
