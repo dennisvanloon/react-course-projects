1bd66c1e369dd4ef5f1b71d1bba040d252e7b7b1
Dennis van Loon <dennis@Ds-MacBook-Pro.local>
2018-07-11 21:14:07 +0200

    after college 17

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -2,83 +2,49 @@
 
 console.log('Indecision app js running');
 
-var app = {
-    title: 'This is the indecision app',
-    subtitle: 'I hope you like it',
-    options: ['One', 'Two']
-};
-
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.options && app.options.length > 0 ? 'Here are your options' : 'No options'
-    ),
-    React.createElement(
-        'ol',
-        null,
-        React.createElement(
-            'li',
-            null,
-            'Item One'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'Item Two'
-        )
-    )
-);
-
 var count = 0;
 var addOne = function addOne() {
-    console.log("addOne");
+    count++;
+    renderCounterApp();
 };
 var minusOne = function minusOne() {
-    console.log("minusOne");
+    count--;
+    renderCounterApp();
 };
 var reset = function reset() {
-    console.log("reset");
+    count = 0;
+    renderCounterApp();
 };
 
-var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        'Count: ',
-        count
-    ),
-    React.createElement(
-        'button',
-        { onClick: addOne },
-        '+1'
-    ),
-    React.createElement(
-        'button',
-        { onClick: minusOne },
-        '-1'
-    ),
-    React.createElement(
-        'button',
-        { onClick: reset },
-        'reset'
-    )
-);
-//console.log(templateTwo);
 var approot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, approot);
+var renderCounterApp = function renderCounterApp() {
+    var templateTwo = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            'Count: ',
+            count
+        ),
+        React.createElement(
+            'button',
+            { onClick: addOne },
+            '+1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: minusOne },
+            '-1'
+        ),
+        React.createElement(
+            'button',
+            { onClick: reset },
+            'reset'
+        )
+    );
+    ReactDOM.render(templateTwo, approot);
+};
+
+renderCounterApp();
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,43 +1,31 @@
 console.log('Indecision app js running');
 
-const app = {
-  title: 'This is the indecision app',
-  subtitle: 'I hope you like it',
-  options: ['One', 'Two']
-};
-
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    <p>{(app.options && app.options.length > 0) ? 'Here are your options' : 'No options'}</p>
-    <ol>
-      <li>Item One</li>
-      <li>Item Two</li>
-    </ol>
-  </div>
-);
-
 let count = 0;
 const addOne = () => {
-    console.log("addOne");
+    count++;
+    renderCounterApp();
 };
 const minusOne = () => {
-    console.log("minusOne");
+    count--;
+    renderCounterApp();
 };
 const reset = () => {
-    console.log("reset");
+    count = 0;
+    renderCounterApp();
 };
 
-const templateTwo = (
-    <div>
-        <h1>Count: {count}</h1>
-        <button onClick={addOne}>+1</button>
-        <button onClick={minusOne}>-1</button>
-        <button onClick={reset}>reset</button>
-    </div>
-);
-//console.log(templateTwo);
 const approot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, approot);
+const renderCounterApp = () => {
+    const templateTwo = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={minusOne}>-1</button>
+            <button onClick={reset}>reset</button>
+        </div>
+    );
+    ReactDOM.render(templateTwo, approot);
+};
+
+renderCounterApp();
