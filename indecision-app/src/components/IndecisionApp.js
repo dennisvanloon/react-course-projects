6debecb91d0fc7c2c5006c4f3ce5cbaa78b6a98b
Dennis van Loon <dennis@Ds-MacBook-Pro.local>
2018-07-22 18:14:47 +0200

    after college 67

--- indecision-app/src/components/IndecisionApp.js
+++ indecision-app/src/components/IndecisionApp.js
@@ -72,15 +72,18 @@ export default class IndecisionApp extends React.Component {
         return (
             <div>
                 <Header subtitle={subtitle} />
-                <Action
-                    hasOptions={this.state.options.length > 0}
-                    handlePick = {this.handlePick} />
-                <Options
-                    options={this.state.options}
-                    handleDeleteOptions={this.handleDeleteOptions}
-                    handleDeleteOption={this.handleDeleteOption} />
-                <AddOption
-                    handleAddOption={this.handleAddOption} />
+                <div className="container">
+                    <Action
+                        hasOptions={this.state.options.length > 0}
+                        handlePick = {this.handlePick} />
+                    <Options
+                        options={this.state.options}
+                        handleDeleteOptions={this.handleDeleteOptions}
+                        handleDeleteOption={this.handleDeleteOption} />
+                    <AddOption
+                        handleAddOption={this.handleAddOption} />
+
+                </div>
                 <OptionModel
                     selectedOption={this.state.selectedOption}
                     handleClearSelectedOption={this.handleClearSelectedOption}
