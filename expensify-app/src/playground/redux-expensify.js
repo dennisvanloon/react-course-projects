b1b609389a6e820b632219ff2df5b71c1bb3af3a
Dennis van Loon <dennis@Ds-MacBook-Pro.local>
2018-07-28 23:19:29 +0200

    after college 92

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -1,5 +1,46 @@
 import {createStore, combineReducers} from 'redux';
 
+// ADD EXPENSE
+// REMOVE EXPENSE
+// EDIT EXPENSE
+// SET TEXT FILTER
+// SORT BY DATE
+// SORT BY AMOUNT
+// SET START DATE
+// SET END DATE
+
+
+// Expenses Reducer
+const expensesReducerDefaultState = [];
+const expensesReducer  = ( state = expensesReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+// Filter Reducer
+const filtersReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
+const filterReducer  = ( state = filtersReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+// Store creation
+const store = createStore(combineReducers({
+    expenses: expensesReducer,
+    filters: filterReducer
+}));
+
+console.log(store.getState());
+
 const demoState = {
     expenses: [{
         id: 'xxdcdccdc',
