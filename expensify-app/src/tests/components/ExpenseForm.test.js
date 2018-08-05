3117916bb480b604d19e8411ede6b98dadd01867
Dennis van Loon <dennis@Ds-MacBook-Pro.local>
2018-08-05 22:02:40 +0200

    after college 122

--- expensify-app/src/tests/components/ExpenseForm.test.js
+++ expensify-app/src/tests/components/ExpenseForm.test.js
@@ -11,4 +11,43 @@ test('Should render expense form correctly', () => {
 test('Should render expense form with expense data correctly', () => {
     const wrapper = shallow(<ExpenseForm expense={expenses[0]}/>);
     expect(wrapper).toMatchSnapshot();
-})
+});
+
+test('Should render error for invalid form submission', () => {
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => {}
+    });
+    expect(wrapper.state('error').length).toBeGreaterThan(0);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('Should set description on description input change', () => {
+    const value = 'New Description';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(0).simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('description')).toBe(value);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('Should set note on textarea change', () => {
+    const value = 'New Note';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('textarea').simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('note')).toBe(value);
+    expect(wrapper).toMatchSnapshot();
+});
+
+test('Should not set amount for invalid input', () => {
+    const value = '12.222';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(1).simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('amount')).toBe('');
+    expect(wrapper).toMatchSnapshot();
+});
