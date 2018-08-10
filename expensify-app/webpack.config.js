5b29b6ed02991242a3482ee50b8d4d3e71692e54
Dennis van Loon <dennis@Ds-MacBook-Pro.local>
2018-08-10 16:58:14 +0200

    after college 133

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -1,28 +1,33 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    console.log('isProduction ', isProduction);
+
+    return {
+        entry: './src/app.js',
+            output: {
         path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            use: [
-              'style-loader',
-              'css-loader',
-              'sass-loader'
-            ],
-            test: /\.s?css$/
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+                test: /\.s?css$/
+            }]
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
     }
 };
